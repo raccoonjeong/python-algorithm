7d62e731856d0e42498af1bff1891091f0d7a22b
raccoonjeong <rana3782@gmail.com>
2021-08-09 21:18:38 +0900

    [백준] 1541.잃어버린 괄호

--- beakjoon/lostBracket.py
+++ beakjoon/lostBracket.py
@@ -1,7 +1,8 @@
 # 내 답안
 arrayWithoutMinus = input().split('-')
 result = 0
-for (index,value) in enumerate(arrayWithoutMinus):
+
+for (index, value) in enumerate(arrayWithoutMinus):
     temp = value.split('+')
     num = 0
     for t in temp:
