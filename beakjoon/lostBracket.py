d25c15632e2077b3e97a546687f88bad9bf51999
raccoonjeong <rana3782@gmail.com>
2021-08-11 22:06:15 +0900

    [백준] 주석추가

--- beakjoon/lostBracket.py
+++ beakjoon/lostBracket.py
@@ -1,11 +1,14 @@
+# 입력 55-50+40
+# 출력 -35
 # 내 답안
+# [(10) , (50 + 30) , (20)]
 arrayWithoutMinus = input().split('-')
 result = 0
 
 for (index, value) in enumerate(arrayWithoutMinus):
     temp = value.split('+')
     num = 0
-    for t in temp:
+    for t in temp: # [50, 30]
         num += int(t)
     if index == 0:
         result += num
--- beakjoon/meetingRoom.py
+++ beakjoon/meetingRoom.py
@@ -1,17 +1,12 @@
 ### 내 풀이 : 시간초과
+
 # n = int(input())
-#
 # arr = [[0,0]] * n
-#
-# for i in range(0, n):
+# for i in range(n):
 #     arr[i] = list(map(int, input().split()))
-#
 # # print(arr)
-#
 # arr.sort(key=lambda x: (x[0], x[1]))
 # print(arr)
-#
-#
 #
 # resultArr = []
 # for a in arr:
@@ -20,25 +15,26 @@
 #     tempArr.append(tempA)
 #
 #     for b in arr:
-#         if (tempA[1] <= b[0]):
+#         # 최초 tempA = [0,6]
+#         if (tempA[1] <= b[0]): # 종료시간이 그다음 회의 시작시간보다 작거나 같으면.. 가능
 #             tempArr.append(b)
 #             tempA = b
 #
-#     resultArr.append(len(tempArr))
+#     resultArr.append(len(tempArr)) # 길이를 담기 위함
 #     # print(tempArr)
 #     # print('-----------')
 #
 # print(max(resultArr))
 
 
-### 답보고 한 것 1 : 56888 KB, 4752 ms
+## 답보고 한 것 1 : 56888 KB, 4752 ms
 # n = int(input())
 # arr = [[0,0]] * n
 #
 # for i in range(0, n):
 #     arr[i] = list(map(int, input().split()))
 #
-# arr.sort(key=lambda x: (x[1], x[0]))
+# arr.sort(key=lambda x: (x[1], x[0])) ## 종료시간 기준으로 정렬하는게 포인트 !!
 #
 # start = 0
 # count = 0
